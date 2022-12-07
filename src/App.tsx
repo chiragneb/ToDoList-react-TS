baac8dbfbea72a5e3d68ab07e9eaf75ab8d6d69b
neb03x <neb.3391@gmail.com>
2022-12-07 12:24:40 +0530

    to upload on github repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }from 'react';
+import { TodoList } from './TodoList';
+import { AddTodoForm } from './AddTodoForm';
+
+const initialTodos:Todo[] = [
+  {
+    text: 'Walk the dog',
+    complete: false,
+  },
+
+  {
+    text: 'Write app',
+    complete: true,
+  },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+  
+const addTodo: AddTodo =(text: string) => {
+  const newTodo = {text, complete: false };
+  setTodos([...todos, newTodo]);
+};
+
+return (
+  <> 
+  <TodoList todos={todos} toggleTodo={toggleTodo} />
+  <AddTodoForm addTodo={addTodo} /> 
+  </>
   );
 }
 
